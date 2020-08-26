7dff7022ff0ae7ede3f4667f28baf8ff00d849f0
Matteo Harchi <matteo.harchi@gmail.com>
2020-08-26 17:11:57 +0200

    Da sistemare
    funziona con un solo nuovo nominativo

--- js/script.js
+++ js/script.js
@@ -11,4 +11,25 @@ var partecipanti = [
     "eta": 46,
 },
 ];
+
+for (var i = 0; i<partecipanti.length; i++){
+    console.log(partecipanti[i].nome + " " + partecipanti[i].cognome);
+};
+for (var i = 0; i < (partecipanti.length - 1); i++) {
+    $("#confirm").click(inserimentoDati);
+}
+
+function inserimentoDati(){
+
+    var newName = $("#name").val();
+    var newSurname = $("#surname").val();
+    var newAge = parseInt($("#age").val());
+    var nuovoUtente = {
+        "nome" : newName,
+        "cognome": newSurname,
+        "età" : newAge,
+    };
+    partecipanti.push(nuovoUtente);
+}
+
 console.log(partecipanti);
