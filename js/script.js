aa0027b65b9c925aa4978d3f1af427c45a2d2f89
Matteo Harchi <matteo.harchi@gmail.com>
2020-08-26 16:21:06 +0200

    Creato array

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var partecipanti = [
+
+{
+    "nome" : "Paolo",
+    "cognome" : "Maldini",
+    "eta": 50,
+},
+{
+    "nome" : "Alessandro",
+    "cognome" : "Nesta",
+    "eta": 46,
+},
+];
+console.log(partecipanti);
